cdaf383723e4454c8b7fea670ed2f4ba27f4fc8e
Ahmad Firaz <ahmadfiraz@gmail.com>
2021-06-16 21:15:46 +0700

    nambahin soal dan function

---
+++ soal.js
@@ -0,0 +1,6 @@
+function cariLurahBaru(calons) {}
+
+console.log(cariLurahBaru([1, 2, 1, 3, 2, 3, 2, 3]));
+console.log(cariLurahBaru([1, 2, 1, 3, 3, 2, 3]));
+console.log(cariLurahBaru([1]));
+console.log(cariLurahBaru([]));
